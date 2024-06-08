447814303b48b00fbbef55dd9125cd816f51e811
mangesh04 <mangeshrpanchal135@gmail.com>
2024-06-08 18:07:15 +0530

    add signup-page frontend implementation

--- frontend/app/(auth-routes)/signup/page.tsx
+++ frontend/app/(auth-routes)/signup/page.tsx
@@ -15,26 +15,35 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { GoogleIcon, GithubIcon } from "@/assets/icons";
 import Link from "next/link";
+
 // -------------
 const formSchema = z.object({
   username: z.string().min(1, {
     message: "username is required",
   }),
-  password: z.string().min(1, {
-    message: "password is required",
-  }),
+
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+
+  password: z.string().min(1, 'password is required').min(8, 'password must have more than 7 characters'),
+
+  confirmPassword: z.string().min(1, "Password confirmation is required"),
+}).refine(data => data.password === data.confirmPassword, {
+  message: 'Passwords do not match',
+  path: ['confirmPassword'],
 });
 
-const Login = () => {
+const Signup = () => {
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      email: "",
       password: "",
+      confirmPassword: "",
     },
   });
 
-  function onSubmit(data: { username: string; password: string }) {
+  function onSubmit(data: { username: string; email: string; password: string; confirmPassword: string }) {
     console.log(data);
   }
 
@@ -43,13 +52,33 @@ const Login = () => {
       className={`min-h-screen bg-violet-50  from-violet-200 via-white to-white flex justify-center items-center p-5 bg-[url('/bg.svg')] bg-no-repeat bg-cover`}
     >
       <div className="max-w-[400px] w-[400px] p-4 shadow-smd border border-border bg-white rounded-sm">
-        <h1 className="text-center text-2xl mb-10 font-medium">Login</h1>
+        <h1 className="text-center text-2xl mb-10 font-medium">Signup</h1>
         <div>
+
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}>
+
               <FormField
                 control={form.control}
                 name="username"
+                render={({ field }) => (
+                  <FormItem className="mb-4">
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="rounded-[4px]"
+                        placeholder="shadcn"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
                 render={({ field }) => (
                   <FormItem className="mb-4">
                     <FormLabel>Email</FormLabel>
@@ -64,6 +93,7 @@ const Login = () => {
                   </FormItem>
                 )}
               />
+
               <FormField
                 control={form.control}
                 name="password"
@@ -82,16 +112,37 @@ const Login = () => {
                   </FormItem>
                 )}
               />
+
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem className="mb-4">
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        className="rounded-[4px]"
+                        placeholder="shadcn"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+
               <Button
                 type="submit"
-                className="w-full mt-4 rounded-[4px] font-semibold"
-              >
-                Login
+                className="w-full mt-4 rounded-[4px] font-semibold">
+                Signup
               </Button>
+
             </form>
           </Form>
 
-          <p className="text-center text-sm text-grey-600 mt-2">already have an account? <Link href={"/signup"} className="text-blue-500 hover:underline">sign up</Link> </p>
+          <p className="text-center text-sm text-grey-600 mt-2">already have an account? <Link href={"/login"} className="text-blue-500 hover:underline">login</Link> </p>
 
           <p className="text-center mt-4">or</p>
           <div>
@@ -110,4 +161,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
