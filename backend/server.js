02a043b71be636241493b0dbb88b34cfbb457857
mdiab1 <moustapha.diab5@gmail.com>
2024-06-10 16:27:21 -0500

    backend server setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/hello', (req, res)=>{
+    res.set('Content-Type', 'text/html');
+    res.status(200).send("<h1>Hello World!</h1>");
+});
+
+app.listen(port, (error) =>{
+    if(!error)
+        console.log(`Server is running on http://localhost:${port}`);
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
